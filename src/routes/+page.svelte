c73a3905bce68ff04c9cdcd4dacfca8d283849e6
Josephguzman03 <j4guzman@ucsd.edu>
2024-02-18 20:08:34 -0800

    basic script

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,15 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<script> 
+import * as d3 from "d3";
+import Charts from "./Chart/Chart.svelte";
+import Bars from "./Chart/Bars.svelte";
+import Axis from "./ChartAxis.svelte";
+
+const formatDate = d3.timeFormate("%-b %-d")
+export let data = [];
+//export let xAcessor = d => d.x;
+export let label;
+
+</script>
