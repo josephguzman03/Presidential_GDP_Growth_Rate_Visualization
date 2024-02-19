424f5945e5168a7bda89dadeeee681e33b918de9
josephguzman03 <j4guzman@ucsd.edu>
2024-02-18 21:37:43 -0800

    data

--- src/components/map.svelte
+++ src/components/map.svelte
@@ -8,7 +8,7 @@ import Axis from "./ChartAxis.svelte";
 const formatDate = d3.timeFormate("%-b %-d")
 
 function main() {
-    d3.csv('/src/components/pres.csv', d3.autoType).then(
+    d3.csv('../data/profies.csv', d3.autoType).then(
         function(d) {
             for (let index = 0; index < d.length; index++) {
                 const element = d[index];
@@ -25,15 +25,14 @@ const margin = { top: 70, right: 30, bottom: 40, left: 80};
 const width = 1200 - margin.left - margin.right;
 const height = 500 - margin.top - margin.bottom;
 
-const x = d3.scaletime().range([0, width]);
-const y = d3.scaleLinear().range([height, 0]);
+// const x = d3.scaletime().range([0, width]);
+// const y = d3.scaleLinear().range([height, 0]);
 
-const svg = d3.select("#chart-container")
-    .append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottoms)
-    .append("g")
-
+// const svg = d3.select("#chart-container")
+//     .append("svg")
+//         .attr("width", width + margin.left + margin.right)
+//         .attr("height", height + margin.top + margin.bottoms)
+//     .append("g")
 
 
 </script>
