13650d069ff848caf5636c8d54c8d5a0beb57fde
josephguzman03 <j4guzman@ucsd.edu>
2024-02-19 16:56:11 -0800

    data

--- src/components/map.svelte
+++ src/components/map.svelte
@@ -5,84 +5,72 @@ import { onMount } from 'svelte';
 const formatDate = d3.timeFormat("%-b %-d");
 
 let presData = [];
-let gdpData = [];
 
  onMount(async () => {
 
-     const res = await fetch('pres.csv'); 
+     const res = await fetch('df.csv'); 
 
      const csv = await res.text();
 
      presData = d3.csvParse(csv, d3.autoType)
 
      console.log(presData);
-
  });
 
- onMount(async () => {
 
-const res = await fetch('world_GDP.csv'); 
+// const margin = { top: 70, right: 30, bottom: 40, left: 80 };
+// const width = 1200 - margin.left - margin.right;
+// const height = 500 - margin.top - margin.bottom;
 
-const csv = await res.text();
+// const x = d3.scaleTime()
+//   .range([0, width]);
 
-gdpData = d3.csvParse(csv, d3.autoType)
+// const y = d3.scaleLinear()
+//   .range([height, 0]);
 
-console.log(gdpData);
+// const svg = d3.select("#chart-container")
+//   .append("svg")
+//     .attr("width", width + margin.left + margin.right)
+//     .attr("height", height + margin.top + margin.bottom)
+//   .append("g")
+//     .attr("transform", `translate(${margin.left},${margin.top})`);
+// const dataset = [
+//   { date: new Date("2022-01-01"), value: 200 },
+//   { date: new Date("2022-02-01"), value: 250 },
+//   { date: new Date("2022-03-01"), value: 180 },
+//   { date: new Date("2022-04-01"), value: 300 },
+//   { date: new Date("2022-05-01"), value: 280 },
+//   { date: new Date("2022-06-01"), value: 220 },
+//   { date: new Date("2022-07-01"), value: 300 },
+//   { date: new Date("2022-08-01"), value: 450 },
+//   { date: new Date("2022-09-01"), value: 280 },
+//   { date: new Date("2022-10-01"), value: 600 },
+//   { date: new Date("2022-11-01"), value: 780 },
+//   { date: new Date("2022-12-01"), value: 320 }
+// ];
 
-});
-const margin = { top: 70, right: 30, bottom: 40, left: 80 };
-const width = 1200 - margin.left - margin.right;
-const height = 500 - margin.top - margin.bottom;
+// x.domain(d3.extent(dataset, d => d.date));
+// y.domain([0, d3.max(dataset, d => d.value)]);
 
-const x = d3.scaleTime()
-  .range([0, width]);
-
-const y = d3.scaleLinear()
-  .range([height, 0]);
-
-const svg = d3.select("#chart-container")
-  .append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-    .attr("transform", `translate(${margin.left},${margin.top})`);
-const dataset = [
-  { date: new Date("2022-01-01"), value: 200 },
-  { date: new Date("2022-02-01"), value: 250 },
-  { date: new Date("2022-03-01"), value: 180 },
-  { date: new Date("2022-04-01"), value: 300 },
-  { date: new Date("2022-05-01"), value: 280 },
-  { date: new Date("2022-06-01"), value: 220 },
-  { date: new Date("2022-07-01"), value: 300 },
-  { date: new Date("2022-08-01"), value: 450 },
-  { date: new Date("2022-09-01"), value: 280 },
-  { date: new Date("2022-10-01"), value: 600 },
-  { date: new Date("2022-11-01"), value: 780 },
-  { date: new Date("2022-12-01"), value: 320 }
-];
-
-x.domain(d3.extent(dataset, d => d.date));
-y.domain([0, d3.max(dataset, d => d.value)]);
-
-svg.append("g")
-  .attr("transform", `translate(0,${height})`)
-  .call(d3.axisBottom(x)
-    .ticks(d3.timeMonth.every(1)) 
-    .tickFormat(d3.timeFormat("%b %Y"))); 
+// svg.append("g")
+//   .attr("transform", `translate(0,${height})`)
+//   .call(d3.axisBottom(x)
+//     .ticks(d3.timeMonth.every(1)) 
+//     .tickFormat(d3.timeFormat("%b %Y"))); 
 
 
-svg.append("g")
-  .call(d3.axisLeft(y));
+// svg.append("g")
+//   .call(d3.axisLeft(y));
 
-const line = d3.line()
-    .x(d => x(d.date))
-    .y(d => y(d.value));
+// const line = d3.line()
+//     .x(d => x(d.date))
+//     .y(d => y(d.value));
 
-svg.append("path")
-  .datum(dataset)
-  .attr("fill", "none")
-  .attr("stroke", "steelblue")
-  .attr("stroke-width", 1)
-  .attr("d", line);
+// svg.append("path")
+//   .datum(dataset)
+//   .attr("fill", "none")
+//   .attr("stroke", "steelblue")
+//   .attr("stroke-width", 1)
+//   .attr("d", line);
 
-</script>
+// </script>
