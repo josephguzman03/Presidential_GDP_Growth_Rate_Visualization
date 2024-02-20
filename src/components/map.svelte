c209622e2e031edb4f95ac0e11d1cc048aceaac4
josephguzman03 <j4guzman@ucsd.edu>
2024-02-19 17:00:55 -0800

    data

--- src/components/map.svelte
+++ src/components/map.svelte
@@ -1,23 +1,75 @@
 <script> 
-import * as d3 from "d3";
-import { onMount } from 'svelte';
-
-const formatDate = d3.timeFormat("%-b %-d");
-
-let presData = [];
-
- onMount(async () => {
-
-     const res = await fetch('df.csv'); 
-
-     const csv = await res.text();
-
-     presData = d3.csvParse(csv, d3.autoType)
-
-     console.log(presData);
- });
-
-
+    import { onMount } from 'svelte';
+    import * as d3 from "d3";
+  
+    const formatDate = d3.timeFormat("%-b %-d");
+  
+    let presData = [];
+    let selectedYear = '';
+  
+    // Function to create the visualization
+    function createVisualization() {
+      const margin = { top: 20, right: 30, bottom: 50, left: 50 };
+      const width = 800 - margin.left - margin.right;
+      const height = 400 - margin.top - margin.bottom;
+  
+      const svg = d3.select("#visualization")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", `translate(${margin.left}, ${margin.top})`);
+  
+      const xScale = d3.scaleTime()
+        .domain(d3.extent(presData, d => d.Date))
+        .range([0, width]);
+  
+      const yScale = d3.scaleLinear()
+        .domain([0, d3.max(presData, d => d.GDP)])
+        .range([height, 0]);
+  
+      const xAxis = d3.axisBottom(xScale)
+        .tickFormat(formatDate);
+  
+      const yAxis = d3.axisLeft(yScale);
+  
+      svg.append("g")
+        .attr("transform", `translate(0, ${height})`)
+        .call(xAxis);
+  
+      svg.append("g")
+        .call(yAxis);
+  
+      svg.selectAll(".dot")
+        .data(presData)
+        .enter().append("circle")
+        .attr("class", "dot")
+        .attr("cx", d => xScale(d.Date))
+        .attr("cy", d => yScale(d.GDP))
+        .attr("r", 5)
+        .style("fill", "steelblue")
+        .append("title")
+        .text(d => `${d.President}\nGDP: ${d.GDP}\nDate: ${formatDate(d.Date)}`);
+    }
+  
+    // Create the visualization on component mount
+    onMount(() => {
+      fetch('df.csv')
+        .then(response => response.text())
+        .then(csv => {
+          presData = d3.csvParse(csv, d3.autoType);
+          createVisualization();
+        });
+    });
+  </script>
+  
+  <svg id="visualization"></svg>
+  
+  <style>
+    .dot {
+      cursor: pointer;
+    }
+  </style>
+<!-- 
 // const margin = { top: 70, right: 30, bottom: 40, left: 80 };
 // const width = 1200 - margin.left - margin.right;
 // const height = 500 - margin.top - margin.bottom;
@@ -73,4 +125,4 @@ let presData = [];
 //   .attr("stroke-width", 1)
 //   .attr("d", line);
 
-// </script>
+// </script> -->
